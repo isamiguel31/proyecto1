3a4cce7619da4bae04d72c21929479ad7a65884d
isamiguel31 <129995151+isamiguel31@users.noreply.github.com>
2023-04-05 17:07:39 -0300

    agregue el proyecto

---
+++ app/Views/plantilla.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" integrity="" crossorigin=""></script>
+    <link href="assets/css/miestilo.css" rel="stylesheet">
+
+        <title>Home</title>
+
+  </head>
+  <body>
+
+    <div class="titulo">
+    <h1>Bienvenidos a "Mate.ando"</h1>
+    </div>
+
+    <div class="parrafo">
+      <p>Mate.ando es una empresa familiar dedicada a la comercialización de mates en sus diferentes variedades</p>
+    </div>
+
+    <script src="assets/js/bootstrap.bundle.min.js" integrity="" crossorigin=""></script>
+  </body>
+
+<footer>
+    Pie de pagina
+</footer>
+
+</html>
